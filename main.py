3867ab03a2769b2edeb6fb968214304e0499380c
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2022-03-07 12:03:04 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import mean
+import median
+import range
+import standard_deviation
+
+def main():
+    x = [0,4,2,6,1,2,5,3,7,8]
+    print(standard_deviation.get(x))
+
+if __name__ == "__main__":
+    main()
+
